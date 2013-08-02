35c4e543738acf791e77d73941e08b00fb0e68a4
Colin Densem <colindensem@summit360.co.uk>
2013-08-02 19:52:09 +0100

    Initial Commit

---
+++ src/controller/cb_tutorial_greeting_controller.erl
@@ -0,0 +1,29 @@
+-module (cb_tutorial_greeting_controller, [Req]).
+-compile(export_all).
+
+hello('GET', []) ->
+  {ok, [{greeting, "Hello, world!"}]}.
+
+list('GET', []) ->
+  Greetings = boss_db:find(greeting, []),
+  {ok, [{greetings, Greetings}]}.
+
+create('GET',[]) ->
+  ok;
+
+create('POST',[]) ->
+  GreetingText = Req:post_param("greeting_text"),
+  NewGreeting = greeting:new(id,GreetingText),
+  case NewGreeting:save() of
+    { ok, SavedGreeting} ->
+     { redirect, [{ action, "list" }]};
+    { error, ErrorList } ->
+    {ok, [{errors, ErrorList}, {new_msg, NewGreeting }]}
+
+  end.
+
+
+
+goodbye('POST', []) ->
+  boss_db:delete(Req:post_param("greeting_id")),
+  {redirect, [{action, "list"}]}.
